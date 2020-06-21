cb29b8b90f7b95e246498d69775cceb834804b4c
stakagi <Shinichi.Takagi@ha.ax>
2020-06-21 14:31:41 +0300

    adding components CreateUser.tsx, DeleteUser.tsx, UpdateUser.tsx

---
+++ pictionarizer-ui/src/components/CreateUser.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import User from '../interfaces/User.interface';
+import UsersDataService from '../api/UsersDataService';
+import { Formik, Form, Field, ErrorMessage } from 'formik'; 
+import IUserProps from '../interfaces/IUserProps.interface';
+import IUserState from '../interfaces/IUserState.interface';
+
+class CreateUser extends React.Component<IUserProps, IUserState>{
+
+  constructor(props: IUserProps){
+    super(props)
+
+    this.state = {
+      userId: this.props.match.params.id,
+      userData: {
+        id: 0,
+        name: '',
+        ownLanguage: '',
+        targetLanguage: '',
+        country: '',
+        email: '',
+        password: '',
+        imageUrl: '',
+        description: '' 
+      }
+    }
+
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  componentDidMount(){
+    // directly mutating the state. to be refined later 
+    this.state.userData.id = null; 
+  }
+
+  onSubmit(values: User){
+    let user = values;
+
+    UsersDataService.createUser(user)
+    .then(() => this.props.history.push('/'))       
+  }
+  
+  render(){
+    let { id, name, ownLanguage, 
+      targetLanguage, country, email, 
+      password, imageUrl, description} 
+      = this.state.userData; 
+
+    return(
+      <div>
+        <h2>Create User</h2>
+        <div>
+          <Formik
+            initialValues={{ id, name, ownLanguage, targetLanguage, 
+              country, email, password, imageUrl, description}}
+            onSubmit={this.onSubmit}
+            enableReinitialize={true}
+          >
+            {
+              (props) => (
+                <Form>
+                  <fieldset>
+                    <label>Name</label>&nbsp;
+                    <Field type="text" name="name"/>
+                  </fieldset>
+                  <fieldset>
+                    <label>Own Language</label>&nbsp;
+                    <Field type="text" name="ownLanguage"/>
+                  </fieldset>
+                  <fieldset>
+                    <label>Target Language</label>&nbsp;
+                    <Field type="text" name="targetLanguage"/>
+                  </fieldset>
+                  <fieldset>
+                    <label>Country</label>&nbsp;
+                    <Field type="text" name="country"/>
+                  </fieldset>
+                  <fieldset>
+                    <label>Description</label>&nbsp;
+                    <Field type="text" size="75" name="description"/>
+                  </fieldset>
+                  <button type="submit">Save</button>
+                </Form>
+              )
+            }      
+          </Formik>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CreateUser; 
---
+++ pictionarizer-ui/src/components/DeleteUser.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import User from '../interfaces/User.interface';
+import UsersDataService from '../api/UsersDataService';
+import IUserProps from '../interfaces/IUserProps.interface';
+import IUserState from '../interfaces/IUserState.interface';
+
+class DeleteUser extends React.Component<IUserProps, IUserState>{
+
+  constructor(props: IUserProps){
+    super(props)
+
+    this.state = {
+      userId: this.props.match.params.id,
+      userData: {
+        id: 0,
+        name: '',
+        ownLanguage: '',
+        targetLanguage: '',
+        country: '',
+        email: '',
+        password: '',
+        imageUrl: '',
+        description: '' 
+      }
+    }
+
+    this.confirmDelete = this.confirmDelete.bind(this)
+    this.cancelDelete = this.cancelDelete.bind(this)
+  }
+
+  componentDidMount(){
+    let id = Number(this.props.match.params.id);
+    let data: User;
+
+    UsersDataService.retrieveUser(id)
+    .then(res => {
+      data = res.data;
+      this.setState({userData:data});
+    }) 
+  }
+
+  confirmDelete(id: number){
+    UsersDataService.deleteUser(id)
+    .then(() => this.props.history.push('/'))       
+  }
+
+  cancelDelete(){
+    this.props.history.push('/')
+  }
+  
+  render(){
+    let id = Number(this.state.userId);
+    let userName = this.state.userData.name;
+
+    return(
+      <div>
+        <h2>Are you sure you want to delete {userName}?</h2>
+        <button onClick={() => this.confirmDelete(id)}>Yes</button>&nbsp;
+        <button onClick={() => this.cancelDelete()}>No</button>
+      </div>
+    )
+  }
+}
+
+export default DeleteUser; 
