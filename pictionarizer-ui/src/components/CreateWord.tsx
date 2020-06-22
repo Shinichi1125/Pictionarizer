1f5518b780ee717cec4aad4cc63311c17e02fd0e
stakagi <Shinichi.Takagi@ha.ax>
2020-06-22 08:39:01 +0300

    Adding component CreateWord.tsx

---
+++ pictionarizer-ui/src/components/CreateWord.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import Word from '../interfaces/Word.interface';
+import WordsDataService from '../api/WordsDataService';
+import { Formik, Form, Field, ErrorMessage } from 'formik'; 
+import IWordProps from '../interfaces/IWordProps.interface';
+import IWordState from '../interfaces/IWordState.interface';
+
+class CreateWord extends React.Component<IWordProps, IWordState>{
+
+  constructor(props: IWordProps){
+    super(props)
+
+    this.state = {
+      wordId: this.props.match.params.id,
+      wordData: {
+        id: 0,
+        ownLangWordName: '',
+        targetLangWordName: '',
+        ownLangExSentence: '',
+        targetLangExSentence: '',
+        createdDate: new Date,
+        imageUrl: ''
+      }
+    }
+
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  componentDidMount(){
+    // directly mutating the state. to be refined later 
+    this.state.wordData.id = null; 
+  }
+
+  onSubmit(values: Word){
+    let word = values;
+
+    WordsDataService.createWord(word)
+    .then(() => this.props.history.push('/'))       
+  }
+  
+  render(){
+    let { id, ownLangWordName, 
+      targetLangWordName, ownLangExSentence, 
+      targetLangExSentence, createdDate, 
+      imageUrl} 
+      = this.state.wordData; 
+
+    return(
+      <div>
+        <h2>Create Word</h2>
+        <div>
+          <Formik
+            initialValues={{ id, ownLangWordName, 
+              targetLangWordName, ownLangExSentence, 
+              targetLangExSentence, createdDate, 
+              imageUrl}}
+            onSubmit={this.onSubmit}
+            enableReinitialize={true}
+          >
+            {
+              (props) => (
+                <Form>
+                  <fieldset>
+                    <label>Word (Target Language)</label>&nbsp;
+                    <Field type="text" name="targetLangWordName"/>
+                  </fieldset>
+                  <fieldset>
+                    <label>Word (Own Language)</label>&nbsp;
+                    <Field type="text" name="ownLangWordName"/>
+                  </fieldset>
+                  <fieldset>
+                    <label>Sentence (Target Language)</label>&nbsp;
+                    <Field type="text" size="75" name="targetLangExSentence"/>
+                  </fieldset>
+                  <fieldset>
+                    <label>Sentence (Own Language)</label>&nbsp;
+                    <Field type="text" size="75" name="ownLangExSentence"/>
+                  </fieldset>
+                  <fieldset>
+                    <label>Date</label>&nbsp;
+                    <Field type="text" name="createdDate"/>
+                  </fieldset>
+                  <button type="submit">Save</button>
+                </Form>
+              )
+            }      
+          </Formik>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CreateWord; 
