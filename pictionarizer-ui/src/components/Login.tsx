b1eb7f69f1ac3b3363dacfb0f0d080b2636c61bf
stakagi <Shinichi.Takagi@ha.ax>
2020-07-24 12:59:21 +0300

    lodash updated in npm

--- pictionarizer-ui/src/components/Login.tsx
+++ pictionarizer-ui/src/components/Login.tsx
@@ -37,6 +37,11 @@ class Login extends React.Component<ILoginInfoProps, ILoginInfoState>{
   async onSubmit(values: LoginInfo){
     await UsersDataService.userLogin(values)
     .then(res =>{
+      console.log("The returned user ID: " + res.data);
+      if(res.data === 0){
+        console.log("The email address and the password dont' match");
+        return; 
+      }
       setLoginId(String(res.data));
     })
     .then(() => this.props.history.push('/users'))
