35f0d93f5563f6c58f9d634aab4864263128a7d1
stakagi <Shinichi.Takagi@ha.ax>
2020-08-10 18:48:48 +0300

    testGetUserReturnOne added to the test

--- pictionarizerapi/src/test/java/com/pictionarizer/controllers/PictionarizerapiUserControllerTests.java
+++ pictionarizerapi/src/test/java/com/pictionarizer/controllers/PictionarizerapiUserControllerTests.java
@@ -8,15 +8,14 @@ import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional; 
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.pictionarizer.model.User;
 import com.pictionarizer.repos.UserRepository;
@@ -46,5 +45,74 @@ public class PictionarizerapiUserControllerTests {
 		// check the return value (expected, actual) 
 		assertEquals(0, actualList.size());
 	}
+	
+	@Test 
+	@DisplayName("Test case where the findAll()'s result is 2")
+	void testFindAllReturnList() {
+		List<User> list = new ArrayList<>();
+		User user1 = new User();
+		User user2 = new User(); 
+		list.add(user1);
+		list.add(user2);
+		
+		// set the mock class's I/O
+		when(repository.findAll()).thenReturn(list);
+		
+		// execute the service
+		List<User> actualList = repository.findAll();
+		
+		// check the number of the mock's method's execution
+		verify(repository, times(1)).findAll();
+		
+		// check the return value (expected, actual) 
+		assertEquals(2, actualList.size());
+	}
+	
+	@Test
+	@DisplayName("Test case where 1 report gets fetched")
+	void testGetUserReturnOne() {
+		// instantiate a user with a default value 
+		User user = new User(); 
+		user.setId(2020);
+		Optional<User> userOpt = Optional.ofNullable(user);
+		
+		// set the mock class's I/O
+		when(repository.findById(2020)).thenReturn(userOpt);
+		
+		// execute the service 
+		Optional<User> userActual = repository.findById(2020);
+		
+		// check the number of the mock's method's executions
+		verify(repository, times(1)).findById(2020); 
+		
+		// check if the report exists 
+		assertTrue(userActual.isPresent());
+	}
+	
+//	@Test 
+//	@DisplayName("Test case where no user can be fetched")
+//	void testGetUserThrowException() {
+//		// set the mock class's I/O
+//		when(repository.findById(0)).thenThrow(new EmptyResultDataAccessException(1));
+//		
+//		// check if ReportNotFoundException gets thrown when a report cannot be fetched
+//		try {
+//			Optional<User> userOpt = Optional.ofNullable(repository.findById(0).get());
+//		} catch(NoSuchElementException e) {
+//			assertEquals(e.getMessage(), "No value present"); 
+//		}
+//	}
+	
+	@Test
+	@DisplayName("Test case where user is deleted")
+	void testDeleteUser() {
+		List<User> list = new ArrayList<>();
+		User user1 = new User();
+		User user2 = new User(); 
+		list.add(user1);
+		list.add(user2);
+		
+	}
+	
 }
 
