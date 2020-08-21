ef1657d04056620a61489b11a0dd5da0f847930a
stakagi <Shinichi.Takagi@ha.ax>
2020-08-21 12:09:44 +0300

    working on the problem of test result bar not displayed

--- pictionarizer-ui/src/interfaces/ILoginInfoProps.interface.ts
+++ pictionarizer-ui/src/interfaces/ILoginInfoProps.interface.ts
@@ -1,5 +1,5 @@
 import { RouteComponentProps } from 'react-router-dom';
 
-export default interface ILoginInfoProps extends RouteComponentProps<{ id: string}>{
-
+export default interface ILoginInfoProps extends RouteComponentProps<{}>{
+  id: string
 }
