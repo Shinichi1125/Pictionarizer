07c1d43d08ec081f4865d2a16cd2d6c76b64bf84
stakagi <Shinichi.Takagi@ha.ax>
2020-11-23 09:39:03 +0200

    domain pictionarizer.work obtained

--- pictionarizer-ui/src/Constants.ts
+++ pictionarizer-ui/src/Constants.ts
@@ -1,6 +1,7 @@
 //export const API_URL = 'http://localhost:8080/pictionarizerservices/api';
 //export const API_URL = 'http://pictionary-test/pictionarizerservices/api';
-export const API_URL = 'http://pictionarizer/pictionarizerservices/api';
+//export const API_URL = 'http://pictionarizer/pictionarizerservices/api';
+export const API_URL = 'http://pictionarizer.work/pictionarizerservices/api';
 
 export const CONFIG = {
   headers: {
